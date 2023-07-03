de8b9afaf7fcce1d7727adb80e5ed81dffa6b890
ritulsingh <ritulsingh00@gmail.com>
2023-07-03 12:59:00 +0530

    Some update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar(props) {
     return (
-        <nav data-bs-theme="dark" className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
                 <Link className="navbar-brand" to="/">{props.title}</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -60,22 +60,22 @@ export default function TextForm() {
   return (
     <>
       <div className="mb-3 container my-3">
-        <div id="liveAlertPlaceholder"></div>
+        <div id="liveAlertPlaceholder" style={{ height: '62px' }}></div>
         <div className='text-wrap'>
           <label htmlFor="text" className="form-label fs-5 fw-bold">Enter Your Text</label>
           <textarea className="form-control" value={text} onChange={changeText} id="text" rows="10"></textarea>
         </div>
         <div className='my-3'>
-          <button type="button" id="liveAlertBtn" className="btn btn-primary mx-1" onClick={copyText}>Copy Text</button>
-          <button type="button" className="btn btn-primary mx-1" onClick={changeUpCase}>Change To Uppercase</button>
-          <button type="button" className="btn btn-primary mx-1" onClick={changeLoCase}>Change To Lowercase</button>
-          <button type="button" className="btn btn-primary mx-1" onClick={handleCapitalize}>Capitalize</button>
-          <button type="button" className="btn btn-danger mx-1" onClick={clearText}>Clear Text</button>
-          <button type="button" className="btn btn-warning mx-1" onClick={speak}>Speak</button>
+          <button disabled={text.length === 0} type="button" id="liveAlertBtn" className="btn btn-primary mx-1" onClick={copyText}>Copy Text</button>
+          <button disabled={text.length === 0} type="button" className="btn btn-primary mx-1 my-1" onClick={changeUpCase}>Change To Uppercase</button>
+          <button disabled={text.length === 0} type="button" className="btn btn-primary mx-1 my-1" onClick={changeLoCase}>Change To Lowercase</button>
+          <button disabled={text.length === 0} type="button" className="btn btn-primary mx-1 my-1" onClick={handleCapitalize}>Capitalize</button>
+          <button disabled={text.length === 0} type="button" className="btn btn-danger mx-1 my-1" onClick={clearText}>Clear Text</button>
+          <button disabled={text.length === 0} type="button" className="btn btn-warning mx-1 my-1" onClick={speak}>Speak</button>
         </div>
-        <p><b>{text.split(' ').filter(function (n) { return n !== '' }).length}</b> words <b>{text.length}</b> characters</p>
+        <p><b>{text.split(/\s+/).filter(function (n) { return n !== '' }).length}</b> words <b>{text.length}</b> characters</p>
         <p>Estimated read time: <b>{Math.ceil(text.split(' ').filter(function (n) { return n !== '' }).length / 150)} minute(s)</b></p>
-        <p><b>Preview Text: </b>{text}</p>
+        <p><b>Preview Text: </b>{text.length > 0 ? text : "Nothing to preview!"}</p>
       </div>
     </>
   )
